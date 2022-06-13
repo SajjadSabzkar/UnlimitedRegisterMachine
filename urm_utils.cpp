8940c601e55cd11ee793e66ee7af8f9399e8a3e8
ssk7867 <ssk7867@yahoo.com>
2022-06-13 09:22:37 +0430

    zero , successor ,copy added

---
+++ urm_utils.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Successor function. see more at README file
+void S(int &s)
+
+{
+
+    s += 1;
+}
+
+// Zero function
+void Zeros(int &n)
+
+{
+    n = 0;
+}
+ // Copy function 
+void Transfor(int &n, int &m)
+{
+    swap(n, m);
+}
+
+
+
